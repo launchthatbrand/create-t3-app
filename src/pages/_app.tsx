f61caf5bd6e23d81da25771047f8f748a90e5f8f
LaunchThatBrand <info@launchthatbrand.com>
2024-01-06 18:30:22 -0500

    added fontawesome package and notus landing page, notus tailwind styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,28 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import "~/styles/tailwind.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
