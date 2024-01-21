241ca291644f8889a15ca3f3033acfec054a0da5
LaunchThatBrand <info@launchthatbrand.com>
2024-01-20 19:29:05 -0500

    changed to used react-firebase-hooks

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,11 +12,16 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { HiEye, HiEyeOff } from "react-icons/hi";
+import React, { useEffect } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import {
+  useCreateUserWithEmailAndPassword,
+  useUpdateProfile,
+} from "react-firebase-hooks/auth";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React from "react";
 import { auth } from "~/lib/firebase/client";
 import { useForm } from "react-hook-form";
 import { useToast } from "@/components/ui/use-toast";
@@ -44,102 +49,189 @@ function LoginPage() {
     },
   });
 
+  const [createUserWithEmailAndPassword, user, loading, createError] =
+    useCreateUserWithEmailAndPassword(auth);
+  const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+  useEffect(() => {
+    if (createError ?? updateError) {
+      const errorToDisplay = createError ?? updateError;
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: `${errorToDisplay?.message}`,
+      });
+    }
+  }, [createError, toast, updateError]);
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values);
+    // console.log(values);
     const { email, password, name } = values;
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredentials) => {
-        void updateProfile(userCredentials.user, {
-          displayName: name,
-        });
-        toast({
-          title: "Registered Sucessfully",
-          description: userCredentials.user.email,
-        });
-      })
-      .catch((error) => {
-        toast({
-          variant: "destructive",
-          title: "Uh oh! Something went wrong.",
-          description: `${error}`,
-        });
-      });
+    const user = await createUserWithEmailAndPassword(email, password);
+    await updateProfile({ displayName: name });
+    console.log("user", user?.user);
+
+    // createUserWithEmailAndPassword( email, password)
+    //   .then((userCredentials) => {
+    //     void updateProfile(userCredentials.user, {
+    //       displayName: name,
+    //     });
+    //     toast({
+    //       title: "Registered Sucessfully",
+    //       description: userCredentials.user.email,
+    //     });
+    //   })
+    //   .catch((error) => {
+    //     toast({
+    //       variant: "destructive",
+    //       title: "Uh oh! Something went wrong.",
+    //       description: `${error}`,
+    //     });
+    //   });
   }
   return (
     <div className="flex min-h-screen flex-1 items-center justify-center">
       <div className="w-64 rounded-md bg-slate-300 p-5 shadow-md">
-        <h3>Login Form</h3>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    This is your public display name.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormDescription>This is your email.</FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <div className="relative">
-                      <Input
-                        type={showPassword ? "text" : "password"}
-                        placeholder="shadcn"
-                        {...field}
-                      />
-                      <div className="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-3 text-gray-400">
-                        {showPassword ? (
-                          <HiEyeOff
-                            className="h-6 w-6"
-                            onClick={togglePasswordVisibility}
+        <Tabs defaultValue="login" className="w-full">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="login">Login</TabsTrigger>
+            <TabsTrigger value="register">Register</TabsTrigger>
+          </TabsList>
+          <TabsContent value="login">
+            <h3>Login Form</h3>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="shadcn" {...field} />
+                      </FormControl>
+                      <FormDescription>This is your email.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <div className="relative">
+                          <Input
+                            type={showPassword ? "text" : "password"}
+                            placeholder="shadcn"
+                            {...field}
                           />
-                        ) : (
-                          <HiEye
-                            className="h-6 w-6"
-                            onClick={togglePasswordVisibility}
+                          <div className="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-3 text-gray-400">
+                            {showPassword ? (
+                              <HiEyeOff
+                                className="h-6 w-6"
+                                onClick={togglePasswordVisibility}
+                              />
+                            ) : (
+                              <HiEye
+                                className="h-6 w-6"
+                                onClick={togglePasswordVisibility}
+                              />
+                            )}
+                          </div>
+                        </div>
+                      </FormControl>
+                      <FormDescription>This is your password.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
+          </TabsContent>
+          <TabsContent value="register">
+            <h3>Register Form</h3>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="shadcn" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        This is your public display name.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="shadcn" {...field} />
+                      </FormControl>
+                      <FormDescription>This is your email.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <div className="relative">
+                          <Input
+                            type={showPassword ? "text" : "password"}
+                            placeholder="shadcn"
+                            {...field}
                           />
-                        )}
-                      </div>
-                    </div>
-                  </FormControl>
-                  <FormDescription>This is your password.</FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit">Submit</Button>
-          </form>
-        </Form>
+                          <div className="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-3 text-gray-400">
+                            {showPassword ? (
+                              <HiEyeOff
+                                className="h-6 w-6"
+                                onClick={togglePasswordVisibility}
+                              />
+                            ) : (
+                              <HiEye
+                                className="h-6 w-6"
+                                onClick={togglePasswordVisibility}
+                              />
+                            )}
+                          </div>
+                        </div>
+                      </FormControl>
+                      <FormDescription>This is your password.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
+          </TabsContent>
+        </Tabs>
       </div>
     </div>
   );
