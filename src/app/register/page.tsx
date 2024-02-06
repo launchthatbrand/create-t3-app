81bb000967c13ef69bea504a9fe8e433de4cb721
LaunchThatBrand <info@launchthatbrand.com>
2024-02-05 20:19:20 -0500

    styled registration page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,9 +3,9 @@ import RegisterForm from "../_components/RegisterForm";
 
 function RegisterPage() {
   return (
-    <div>
-      <div>RegisterPage</div>
-      <RegisterForm />;
+    <div className="bg-white p-5 rounded-md shadow-md text-black space-y-5">
+      <h3 className="text-center font-medium">Register Page</h3>
+      <RegisterForm />
     </div>
   );
 }
