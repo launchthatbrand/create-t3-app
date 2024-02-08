becb162f86a00dcb360b6aeff7447893c5d81640
LaunchThatBrand <info@launchthatbrand.com>
2024-02-08 14:46:13 -0500

    lint fix

--- src/app/supabase/posts/page.tsx
+++ src/app/supabase/posts/page.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import supabaseServer from "~/lib/supabase/server";
 
+interface Post {
+  name: string;
+}
+
 async function page() {
   const supabase = await supabaseServer();
-  const { data, error } = await supabase.from("post").select("*");
+  const result = await supabase.from("post").select("*");
+
+  const data = result.data as Post[];
+
   console.log("result", data);
   return (
     <div className="w-">
