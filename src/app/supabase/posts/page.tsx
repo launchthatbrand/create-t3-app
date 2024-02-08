26c83098d0e19aba00d0dea615c218bc7d2e6916
LaunchThatBrand <info@launchthatbrand.com>
2024-02-08 14:23:37 -0500

    added supabase posts page

--- src/app/supabase/posts/page.tsx
+++ src/app/supabase/posts/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import supabaseServer from "~/lib/supabase/server";
 
-function page() {
-  return <div>Supabase Posts</div>;
+async function page() {
+  const supabase = await supabaseServer();
+  const { data, error } = await supabase.from("post").select("*");
+  console.log("result", data);
+  return (
+    <div className="w-">
+      Supabase Posts
+      {data?.map((item, index) => (
+        <div
+          className="rounded-md bg-white bg-opacity-10 p-3 shadow-md"
+          key={index}
+        >
+          {item.name}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default page;
