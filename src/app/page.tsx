4feb6dd2c35f36596f6b509821e20fb0b6e71453
LaunchThatBrand <info@launchthatbrand.com>
2024-02-08 12:23:55 -0500

    updated planetscale connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,7 @@ export default async function Home() {
 
 async function CrudShowcase() {
   const session = await getServerAuthSession();
-  if (!session?.user) return null;
+  // if (!session?.user) return null;
 
   const latestPost = await api.post.getLatest.query();
 
