358f7bde599ec93d956f86ef9384e1f7a2b6a2ca
Desmond <desmond@Desmonds-MacBook-Pro.local>
2024-02-11 21:11:18 -0500

    siwe working

--- src/app/_components/ClientProvider.tsx
+++ src/app/_components/ClientProvider.tsx
@@ -1,8 +1,31 @@
+/* eslint-disable @typescript-eslint/require-await */
 "use client";
 import { WagmiProvider, createConfig, http } from "wagmi";
+import { SiweMessage } from "siwe";
 import { mainnet } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import {
+  ConnectKitProvider,
+  type SIWEConfig,
+  SIWEProvider,
+  getDefaultConfig,
+  type SIWESession,
+} from "connectkit";
+
+async function afterSiweSignin(session: SIWESession | undefined) {
+  if (!session) return Error;
+  try {
+    console.log("afterSiweSigninStart", session);
+    // const token = await firebaseAdminCreateCustomToken({
+    //   uid: session.address,
+    // });
+    // console.log("afterSiweSigninToken", token);
+    // const user = await signInWithCustomToken(auth, token);
+    // console.log("afterSiweSigninUser", user);
+  } catch (error) {
+    console.log("afterSiweSigninError", error);
+  }
+}
 
 const config = createConfig(
   getDefaultConfig({
@@ -28,13 +51,53 @@ const config = createConfig(
   }),
 );
 
+const siweConfig = {
+  getNonce: async () => {
+    const res = await fetch(`/siwe`, { method: "PUT" });
+    if (!res.ok) throw new Error("Failed to fetch SIWE nonce");
+
+    return res.text();
+  },
+  createMessage: ({ nonce, address, chainId }) => {
+    return new SiweMessage({
+      nonce,
+      chainId,
+      address,
+      version: "1",
+      uri: window.location.origin,
+      domain: window.location.host,
+      statement: "Sign In With Ethereum to prove you control this wallet.",
+    }).prepareMessage();
+  },
+  verifyMessage: ({ message, signature }) => {
+    return fetch(`/siwe`, {
+      method: "POST",
+      body: JSON.stringify({ message, signature }),
+      headers: { "Content-Type": "application/json" },
+    }).then((res) => res.ok);
+  },
+  getSession: async () => {
+    const res = await fetch(`/siwe`);
+    if (!res.ok) throw new Error("Failed to fetch SIWE session");
+
+    const { address, chainId } = (await res.json()) as SIWESession;
+    return address && chainId ? { address, chainId } : null;
+  },
+  signOut: () => fetch(`/siwe`, { method: "DELETE" }).then((res) => res.ok),
+} satisfies SIWEConfig;
+
 const queryClient = new QueryClient();
 
 export const Web3Provider = ({ children }: { children: React.ReactNode }) => {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <ConnectKitProvider>{children}</ConnectKitProvider>
+        <SIWEProvider
+          {...siweConfig}
+          onSignIn={(session?: SIWESession) => afterSiweSignin(session)}
+        >
+          <ConnectKitProvider>{children}</ConnectKitProvider>
+        </SIWEProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
